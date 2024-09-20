dbaf8371e191ca1a0fa13db6e807852ee66453da
Simmi Keshri <b7wuuw2o@students.codeinstitute.net>
2024-09-20 11:19:31 +0000

    add summernote

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
+from . import views
+from django.urls import path
 
-from django.urls import path, include
-from .views import *
 urlpatterns = [
-    
+   path('', views.PostList.as_view(), name='home'), 
 
 ]
