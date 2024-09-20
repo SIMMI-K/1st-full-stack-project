8c7a9d38442270e3fe80cb6b7882d30224b38e7d
Simmi Keshri <b7wuuw2o@students.codeinstitute.net>
2024-09-20 22:08:31 +0000

    add cloudinary

--- dancezone/settings.py
+++ dancezone/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-simmik-1stfullstackproj-k0uzb3xcbcm.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'django_summernote',
     'blog',
 ]
