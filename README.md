8c92868751da046e5764252631a87adee8036427
Simmi Keshri <b7wuuw2o@students.codeinstitute.net>
2024-09-29 20:01:39 +0000

    fixing bugs and update user stories in readme file

--- README.md
+++ README.md
@@ -14,3 +14,79 @@ DanceZone began as a blog dedicated to exploring the vibrant and diverse world o
 The DanceZone website offers a seamless experience for users, featuring a blog that covers various dance styles, tips for beginners and seasoned dancers alike, and cultural insights into dance traditions worldwide. Registered users can also enjoy the convenience of booking dance classes directly through the website. The app allows users to schedule classes for different styles—from classical forms like Bharatanatyam and Ballet to contemporary styles like Hip Hop and Salsa. 
 
 DanceZone’s goal is to inspire, educate, and connect dance enthusiasts while offering a streamlined platform to learn and practice with expert instructors. Whether you're here to read, learn, or dance, DanceZone is your ultimate destination for everything dance!
+
+## Table of contents
+
+- [Epics and user stories](#epics-and-user-stories)
+   * [Epics](#epics)
+   * [User stories](#user-stories)
+
+### Epics
+
+1. Fully functioning home page
+2. Database and admin setup
+3. Register page and form
+4. Login page and form
+5. Navigation bar that looks different when logged in
+6. Blog detail page that has different capabilities when logged in compared to when not logged 
+   in
+7. Log out page
+8. Booking page for various dance styles, that can be only accessed when logged in. This 
+   includes full front-end CRUD functionality for the user
+
+### User stories
+
+Open a post: As a Site User, I can click on a post so that I can read the full text.
+
+AC1 When a blog post title is clicked on a detailed view of the post is seen.
+
+View comments: As a Site User / Admin I can view comments on an individual post so that I can read the conversation
+
+AC1 Given one or more user comments the admin can view them.
+AC2 Then a site user can click on the comment thread to read the conversation.
+
+Account registration: As a Site User I can register an account so that I can comment on a post.
+
+AC1 Given an email a user can register an account.
+AC2 Then the user can log in.
+AC3 When the user is logged in they can comment.
+
+Comment on a post: As a Site User I can leave comments on a post so that I can be involved in the conversation
+
+AC1 When a user comment is approved
+AC2 Then a user can reply
+AC3 Given more than one comment then there is a conversation thread
+
+Modify or delete comment on a post: As a Site User I can modify or delete my comment on a post so that I can be involved in the conversation
+
+AC1 Given a logged in user, they can modify their comment
+AC2 Given a logged in user, they can delete their comment
+
+Booking of dance class: As a Site user I can book a class so that I can enjoy the dance style I want from the available dance style
+
+AC1 As a user I should be able to choose from dance list available
+AC2 As a user I Could choose from the date available to join the dance
+AC3 As a user i could choose from the time slots available
+
+Edit and delete dance class: As a SITE USER I can edit booking so that I Can change my choice accordingly
+
+AC1 As a user I Can edit my preference
+AC2 As a user I Can delete my booking if made mistake
+AC3 As a user I Can view my bookings
+
+Manage posts: As a Site Admin I can create, read, update and delete posts so that I can manage my blog content
+
+AC1 Given a logged in user, they can create a blog post
+AC2 Given a logged in user, they can read a blog post
+AC3 Given a logged in user, they can update a blog post
+AC4 Given a logged in user, they can delete a blog post
+
+Create drafts: As a Site Admin I can create draft posts so that I can finish writing the content later
+
+AC1 Given a logged in user, they can save a draft blog post
+AC2 Then they can finish the content at a later time
+
+Approve comments: As a Site Admin I can approve or disapprove comments so that I can filter out objectionable comments
+
+AC1 Given a logged in user, they can approve a comment
+AC2 Given a logged in user, they can disapprove a comment
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.views.generic import DeleteView
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
+from .models import Post, Comment
 from .forms import CommentForm
 
-# Create your views here.
+# Views
+
+"""
+Credit: https://github.com/Code-Institute-Solutions/blog/blob/main/12_
+views_part_3/05_edit_delete/blog/views.py
+"""
+
+
 class PostList(generic.ListView):
-     """
+    """
     Returns all published posts in :model:`blog.Post`
     and displays them in a page of six posts.
+
     **Context**
 
     ``queryset``
@@ -21,24 +34,30 @@ class PostList(generic.ListView):
     :template:`blog/index.html`
     """
 
-     queryset = Post.objects.filter(status=1)
-     template_name = "blog/index.html"
-     paginate_by = 6
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
+
 
 def post_detail(request, slug):
     """
-    Display an individual :model:`blog.Post`.
+    Display an individual item from :model:`blog.Post`.
 
     **Context**
 
     ``post``
         An instance of :model:`blog.Post`.
+    ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
 
     **Template:**
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
@@ -56,16 +75,123 @@ def post_detail(request, slug):
                 messages.SUCCESS,
                 "Comment submitted and" + " awaiting approval",
             )
-    comment_form = CommentForm()
+            return HttpResponseRedirect(reverse("post_detail", args=[slug]))
+    else:
+        comment_form = CommentForm()
 
     return render(
-    request,
-    "blog/post_detail.html",
-    {
-        "post": post,
-        "comments": comments,
-        "comment_count": comment_count,
-        "comment_form": comment_form,
-    },
-)
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+        },
+    )
 
+
+@login_required
+def comment_edit(request, slug, comment_id):
+    """
+    Display an individual comment for edit and handles its update
+
+    Uses :model: `blog.Comment` and :model: `blog.Post`
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+    ``comment``
+        A single comment related to the post.
+    ``comment_form``
+        An instance of :form:`blog.CommentForm`
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+    if request.method == "POST":
+
+        if "reset-button-comments" in request.POST:
+            return HttpResponseRedirect(reverse("post_detail", args=[slug]))
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, "Comment Updated!")
+        else:
+            messages.add_message(request, messages.ERROR, "Error updating"
+                                 + "comment!")
+
+    return HttpResponseRedirect(reverse("post_detail", args=[slug]))
+
+
+"""
+Credits for delete class below
+Credit 1: https://www.youtube.com/
+watch?v=JzDBCZTgVyw&list=PLXuTq6OsqZjbCSfiLNb2f1FOs8viArjWy&index=14
+Credit 2: https://github.com/Dee-McG/Recipe-Tutorial/blob/main/
+recipes/views.py#L72
+Credit 3: https://github.com/DanMorriss/nialls-barbershop/blob/
+main/booking_system/views.py#L272
+Note: The test function below needed to be added,
+because you cannot use the UserPassesTestMixin without it
+"""
+
+
+class DeleteComment(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    Displays a new page to confirm deletion of a comment
+
+    Uses :model: `blog.Comment`
+
+    **Context**
+    ``comment``
+        A single comment related to the post.
+    ``form``
+        An instance of :form:`blog.CommentForm`
+
+    **Template:**
+
+    :template:`blog/comment_confirm_delete.html`
+
+    The test function is needed for the UserPassesTestMixin.
+    It returns True or False.
+    If True, the item will delete as intended.
+    If False, it will throw a 403 error. A 403 template
+    was made for this instance.
+    """
+
+    model = Comment
+
+    def get_success_url(self):
+        comment = self.get_object()
+        return reverse_lazy("post_detail", kwargs={"slug": comment.post.slug})
+
+    def form_valid(self, form):
+        messages.success(
+            self.request,
+            "Your comment has been deleted successfully!",
+            extra_tags="alert alert-success alert-dismissible",
+        )
+
+        return super().form_valid(form)
+
+    def test_func(self):
+        """
+        Checks if the logged-in user is the author of the comment.
+
+        Returns:
+            bool: True if the logged in user is the author of the comment
+            False otherwise.
+        """
+        comment = self.get_object()
+        return self.request.user == comment.author
